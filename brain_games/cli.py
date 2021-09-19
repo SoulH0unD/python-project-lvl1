8145a438f05e672c0a81d7282cab19ef8c6b13aa
Olemskoy Michael <desed@mail.ru>
2021-09-19 13:23:20 +0300

    modified:   brain_games/cli.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,5 +2,5 @@ import prompt
 
 def welcome_user():
     print("Welcome to the Brain Games!")
-    name_user = prompt.string('May I have your name?')
+    name_user = prompt.string('May I have your name? ')
     print('Hello, ', name_user)

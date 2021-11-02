24d434685bb900bd1026d9abdc4baab629dc63da
Olemskoy Michael <desed@mail.ru>
2021-11-02 12:36:57 +0300

    Add new games brain-calc

---
+++ brain_games/function_brain_games.py
@@ -0,0 +1,18 @@
+import prompt
+
+def acquaintance_users() -> str:
+    """Знакомство с пользователем"""
+    print('Welcome to the Brain Games!')
+    name_user = prompt.string('May I have your name? ')
+    print('Hello, ', name_user, '!')
+    return name_user
+
+def check_the_answer(answer_user, correct_answer, name_user) -> bool:
+    """Проверка ответа пользователя"""
+    if answer_user == correct_answer:
+        print('Correct!')
+        return True
+    else:
+        print(answer_user, ' is wrong answer ;(. Correct answer was ', correct_answer)
+        print('Let`s try again, ', name_user, '!')
+        exit()
